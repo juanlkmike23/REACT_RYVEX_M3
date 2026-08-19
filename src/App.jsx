cd08a643142a6e4b2fe7f8f6432603883916a850
Andres Balleteros <aloboballesteros2008@gmail.com>
2026-08-18 23:05:00 -0500

    Cambios andres

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Header from './components/Header';
 import Hero from './components/Hero';
+import Categorias from './components/Categorias'
 import './App.css';
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <>
       <Header />
       <Hero />
+      <Categorias/>
     </>
   );
 }
